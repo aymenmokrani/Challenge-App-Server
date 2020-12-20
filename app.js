190e227ca11d107bece2c7b0dcbd806ff485e4b6
algerieno <aymen.algerieno2014@gmail.com>
2020-12-20 18:10:48 +0100

    connection with mongo

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
 const apiRoutes = require('./routes/apiRoutes')
+const mongoose = require('mongoose')
+
 
 // routes
 app.use('/api', apiRoutes)
+
+
+// MongoDb Connection
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+mongoose.connection.once('open', () => {
+    console.log("Connection with MongoDb has been established !")
+}).on('error', (error) => console.log(error))
+
 
 
 app.get('/', (req, res) => {
